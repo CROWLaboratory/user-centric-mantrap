0e8cb66fe29fcaeb2d8a93d24d889b8153f68dd1
xholmes <xholmes80@gmail.com>
2014-09-08 23:31:09 +1200

    Update udpBlocking.c

--- udpBlocking.c
+++ udpBlocking.c
@@ -1,6 +1,9 @@
 /*
 
- Copyright (c) 2013, 2014    CROW - Cybersecurity Researchers of Waikato
+ Copyright (c) 2013, 2014    
+ 
+ Authors: Ryan K.L. Ko, Alan Y.S. Tan, Ting Gao
+ CROW - Cybersecurity Researchers of Waikato
 
  All rights reserved.
 
